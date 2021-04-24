5b50bca9dbaa11cbfd9fe6c09d34a6a70fdd6b7c
Sudarshan Naidu <sudarshan.naidu@sts.local>
2021-04-25 00:51:28 +0530

    first commit

---
+++ src/main/java/StartDriverSession.java
@@ -0,0 +1,25 @@
+import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.remote.MobileCapabilityType;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import java.io.File;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class StartDriverSession {
+    public static void main(String args[]) throws MalformedURLException {
+        DesiredCapabilities caps=new DesiredCapabilities();
+        caps.setCapability(MobileCapabilityType.PLATFORM_NAME, "Android");
+        caps.setCapability(MobileCapabilityType.DEVICE_NAME,"Pixel 4");
+        caps.setCapability(MobileCapabilityType.AUTOMATION_NAME,"UiAutomator2");
+        caps.setCapability(MobileCapabilityType.UDID,"emulator-5554");
+        caps.setCapability("avd", "Pixel_XL_API_30");
+        caps.setCapability("avdLaunchTimeout",360000);
+        String appUrl=System.getProperty("user.dir") + File.separator + "src" + File.separator + "main"
+                + File.separator +"resources" + File.separator +"ApiDemos-debug.apk";
+        caps.setCapability(MobileCapabilityType.APP,appUrl);
+        URL url= new URL("http://0.0.0.0:4723/wd/hub");
+        AppiumDriver driver=new AndroidDriver(url,caps);
+    }
+}
